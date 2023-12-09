efbdc1f0b2d9222eaf2af0accbb010627747b99e
c0mplexdev <105768978+c0mplexdevelop@users.noreply.github.com>
2023-12-09 18:22:32 +0800

    Custom auth state provider for auth

---
+++ Auth/UserAccountService.cs
@@ -0,0 +1,17 @@
+﻿namespace Iskolaryo.Auth
+{
+    public class UserAccountService
+    {
+        private List<UserAccount> _userAccounts =
+        [
+            new UserAccount { Username = "admin", Password = "admin", Role = "Admin" },
+            new UserAccount { Username = "user", Password = "user", Role = "User" },
+            new UserAccount { Username = "guest", Password = "guest", Role = "Guest"}
+        ];
+
+        public UserAccount? GetUserAccount(string username)
+        {
+            return _userAccounts.FirstOrDefault(u => u.Username == username);
+        }
+    }
+}
