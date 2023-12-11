13c6cc99814b45c120f7df226673782de5f76c85
c0mplexdev <105768978+c0mplexdevelop@users.noreply.github.com>
2023-12-11 19:53:25 +0800

    new line yay

--- Program.cs
+++ Program.cs
@@ -24,6 +24,7 @@ builder.Services
     .AddFontAwesomeIcons();
 
 builder.Services.AddSignalR(builder => builder.MaximumReceiveMessageSize = 1000000000);
+
 builder.Services.AddServerSideBlazor();
 
 builder.Services.AddAuthenticationCore();
